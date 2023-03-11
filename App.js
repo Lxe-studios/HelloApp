e09b59837aff68c868a1f2db2ccbcbdcac1c9c0b
李雪儿 <>
2023-03-11 15:14:23 +0800

    3.11提交

--- App.js
+++ App.js
@@ -1,16 +1,16 @@
-import React, { useState } from 'react';
-import { Button, View } from 'react-native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { NavigationContainer } from '@react-navigation/native';
+import React, {useState} from 'react';
+import {Button, View} from 'react-native';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import {NavigationContainer} from '@react-navigation/native';
 import WelcomeScreen from './js/pages/Welcome';
 import LoginScreen from './js/pages/auth/Login';
 import RegistScreen from './js/pages/auth/Regist';
 
-function HomeScreen({ navigation }) {
-  const {isLoading,setisLoading} = useState(false)
-  const {userToken,setuserToken} = useState(null)
+function HomeScreen({navigation}) {
+  const {isLoading, setisLoading} = useState(false);
+  const {userToken, setuserToken} = useState(null);
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Button
         onPress={() => navigation.navigate('Notifications')}
         title="Go to notifications"
@@ -19,9 +19,9 @@ function HomeScreen({ navigation }) {
   );
 }
 
-function NotificationsScreen({ navigation }) {
+function NotificationsScreen({navigation}) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Button onPress={() => navigation.goBack()} title="Go back home" />
     </View>
   );
@@ -34,19 +34,19 @@ const myDrawer = () => {
     <NavigationContainer>
       <Stack.Navigator>
         {isLoading ? (
-          <Stack.Screen name='welcome' component={WelcomeScreen}></Stack.Screen>
+          <Stack.Screen name="welcome" component={WelcomeScreen} />
         ) : userToken == null ? (
           <>
-            <Stack.Screen name='login' component={LoginScreen}></Stack.Screen>
-            <Stack.Screen name='redist' component={RegistScreen}></Stack.Screen>
+            <Stack.Screen name="login" component={LoginScreen} />
+            <Stack.Screen name="redist" component={RegistScreen} />
           </>
         ) : (
           <>
-            <Stack.Screen 
-              name='home'
+            <Stack.Screen
+              name="home"
               component={Home}
               option={{headerShown: false}}
-            ></Stack.Screen>
+            />
           </>
         )}
         <Stack.Screen name="Home" component={HomeScreen} />
@@ -54,9 +54,6 @@ const myDrawer = () => {
       </Stack.Navigator>
     </NavigationContainer>
   );
-}
+};
 
-export default myDrawer
-
-
-
+export default myDrawer;
